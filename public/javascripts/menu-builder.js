55dc7850357f481830349294f5efc6fd6e059030
Lisa Carlson <lisacarlson@Maxs-MacBook-Pro.local>
2016-04-06 09:16:28 -0600

    fixed delete and update functions, updated styling

--- public/javascripts/menu-builder.js
+++ public/javascripts/menu-builder.js
@@ -6,40 +6,66 @@ var MenuBuilder = {
 		var that = this;
 		this.data = obj.data;
 
-		$('.update-menu-items').click(function(){
+		$('.update-menu-items').click(function(e){
+			e.preventDefault();
 			that.save();
 		});
-		$('.add-menu-item').click(function(){
-			that.save();
+		$('.add-menu-item').click(function(e){
+			e.preventDefault();
+			that.save(true);
+		});
+		$('.delete-menu-item').click(function(e){
+			e.preventDefault();
+			that.delete(e);
 		});
 	},
 	save: function(isNew){
 		var menuData = [];
 		var valid = true;
 		$('.menu-item').each(function(){
-			var name = $(this).find('.menu-item-name').val();
+			var $menuItemName = $(this).find('.menu-item-name');
+			var name = $menuItemName.val();
 			var content = $(this).find('.menu-item-content').val();
 			if($.trim(name) == ""){
-				valid = false;
-				alert("Please enter a valid name");
-				return;
+				name = $menuItemName.attr('data-menu-item-name');
 			}
 			var item = {name:name, content:content};
 			menuData.push(item);
 		});
 
-		if(valid){
-			var form = $('<form></form>');
-
-		    form.attr("method", "post");
-		    form.attr("action", this.options.path);
-
-		    form.append("<textarea style='visibility:hidden;' name='menu'>" + JSON.stringify(menuData) + "</textarea>");
-
-		    // The form needs to be a part of the document in
-		    // order for us to be able to submit it.
-		    $(document.body).append(form);
-		    form.submit();
+		if(isNew){
+			var $menuItem = $('.menu-new-item');
+			var $menuItemName = $menuItem.find('.menu-new-item-name');
+			var name = $menuItemName.val();
+			var content = $menuItem.find('.menu-new-item-content').val();
+			if($.trim(name) == ""){
+				valid = false;
+				alert("Please enter a valid name.");
+				return;
+			}
+			var item = {name:name, content:content};
+			menuData.push(item);
 		}
+
+		if(valid){
+			this.data = menuData;
+			this.submit();
+		}
+	},
+	delete: function(e){
+		var $deleteButton = $(e.currentTarget);
+		var index = parseInt($deleteButton.attr('data-item-index'));
+		if(confirm("Are you sure you want to delete this menu item?")){
+			this.data.splice(index, 1);
+			this.submit();
+		}
+	},
+	submit: function(){
+		var form = $('<form></form>');
+		form.attr("method", "post");
+		form.attr("action", this.options.path);
+		form.append("<textarea style='visibility:hidden;' name='menu'>" + JSON.stringify(this.data) + "</textarea>");
+		$(document.body).append(form);
+		form.submit();
 	}
 };
